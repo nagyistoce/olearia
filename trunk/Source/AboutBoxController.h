47baae4ae6c9f76a61c70e3e26b3f7d8f6cfa71d
kierene <kierene@20363dd6-5b53-0410-b173-fd8f55d8638a>
2008-09-08 14:32:09 +0000

    - Added Slow Scroll to the About Credits. - Added GPL Blurb to AboutController Files

--- trunk/Source/AboutBoxController.h
+++ trunk/Source/AboutBoxController.h
@@ -5,6 +5,23 @@
 //  Created by Kieren Eaton on 7/09/08.
 //  Copyright 2008 BrainBender Software. All rights reserved.
 //
+//  Created by Kieren Eaton on 25/07/08.
+//  Copyright 2008 BrainBender Software. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 
 #import <Cocoa/Cocoa.h>
 
--- trunk/Source/AboutBoxController.m
+++ trunk/Source/AboutBoxController.m
@@ -5,6 +5,22 @@
 //  Created by Kieren Eaton on 7/09/08.
 //  Copyright 2008 BrainBender Software. All rights reserved.
 //
+//  Created by Kieren Eaton on 25/07/08.
+//  Copyright 2008 BrainBender Software. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import "AboutBoxController.h"
 
@@ -16,6 +32,9 @@
 	if (![super initWithWindowNibName:@"About"]) return nil;
 	
 	
+	currentPosition = 0;
+	restartAtTop = NO;
+	startTime = [NSDate timeIntervalSinceReferenceDate] + 3.0;
 	
 	
 	
@@ -24,6 +43,8 @@
 
 - (void)windowDidLoad
 {
+	[creditsView scrollPoint:NSMakePoint( 0, 0 )];
+	
 	[aboutPanel makeKeyAndOrderFront:self];
 	
 	NSDictionary *infoDict = [[NSBundle mainBundle] infoDictionary];
@@ -50,6 +71,62 @@
 												    documentAttributes:nil]];
 	
 	
+	
+	maxScrollHeight = [[creditsView string] length];
+ 
+	
+}
+
+- (void)windowDidBecomeKey:(NSNotification *)notification
+{
+    scrollTimer = [NSTimer scheduledTimerWithTimeInterval:1/4 
+												   target:self 
+												 selector:@selector(scrollCredits:) 
+												 userInfo:nil 
+												  repeats:YES];
+	
+	
+}
+
+- (void)windowDidResignKey:(NSNotification *)notification
+{
+    [scrollTimer invalidate];
+}
+
+- (void)scrollCredits:(NSTimer *)timer
+{
+	
+    if ([NSDate timeIntervalSinceReferenceDate] >= startTime)
+    {
+		if (restartAtTop)
+        {
+            // Reset the startTime
+            startTime = [NSDate timeIntervalSinceReferenceDate] + 3.0;
+            restartAtTop = NO;
+            
+            // Set the position
+            [creditsView scrollPoint:NSMakePoint( 0, 0 )];
+            
+            return;
+        }
+		if (currentPosition >= maxScrollHeight) 
+        {
+            // Reset the startTime
+            startTime = [NSDate timeIntervalSinceReferenceDate] + 3.0;
+            
+            // Reset the position
+            currentPosition = 0;
+            restartAtTop = YES;
+        }
+        else
+        {
+            // Scroll to the position
+            [creditsView scrollPoint:NSMakePoint( 0, currentPosition )];
+            
+            // Increment the scroll position
+            currentPosition += 0.005;
+        }
+    }
 }
 
 
