268245c1ce158c76598b6b8798733ae98aee5dcf
circledev <circledev@20363dd6-5b53-0410-b173-fd8f55d8638a>
2008-12-01 08:31:45 +0000

    - Added missing Licensing info to a few new classes

--- Frameworks/TalkingBook Framework/Source/BBSTBInfoItem.h
+++ Frameworks/TalkingBook Framework/Source/BBSTBInfoItem.h
@@ -3,7 +3,20 @@
 //  BBSTalkingBook
 //
 //  Created by Kieren Eaton on 30/11/08.
-//  Copyright 2008 . All rights reserved.
+//  Copyright 2008 BrainBender Software. All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #import <Foundation/Foundation.h>
