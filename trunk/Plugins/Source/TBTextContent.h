dee32454145637623826eac6348cae4be5370c28
circledev <circledev@20363dd6-5b53-0410-b173-fd8f55d8638a>
2009-07-14 03:13:52 +0000

    - Forgot to commit the new textOnly Navigation controller class

---
+++ trunk/Plugins/Source/TBTextContent.h
@@ -0,0 +1,16 @@
+//
+//  TBTextContent.h
+//  StdDaisyFormats
+//
+//  Created by Kieren Eaton on 13/07/09.
+//  Copyright 2009 BrainBender Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TBTextContent : NSObject {
+
+}
+
+@end
---
+++ trunk/Plugins/Source/TBTextContent.m
@@ -0,0 +1,14 @@
+//
+//  TBTextContent.m
+//  StdDaisyFormats
+//
+//  Created by Kieren Eaton on 13/07/09.
+//  Copyright 2009 BrainBender Software. All rights reserved.
+//
+
+#import "TBTextContent.h"
+
+
+@implementation TBTextContent
+
+@end
